ef0dd0c52db7df1568e940874e22a0de3abba02b
FoleyLab <jayfoleyiv@gmail.com>
2025-10-07 17:17:32 -0400

    updated some nitrobenzene

---
+++ Mapol/LiH/qed_fci/r_1.72_calculations/LiH_r_1.72_no_coupling.py
@@ -0,0 +1,84 @@
+# set path to the directory where helper_PFCI.py is located
+import sys
+sys.path.append("/Users/jfoley19/Code/qed-ci/src")
+
+# import helper libraries
+import numpy as np
+import psi4
+from helper_PFCI import PFHamiltonianGenerator
+import json
+
+# set precision for printing numpy arrays
+np.set_printoptions(threshold=sys.maxsize)
+
+# create file prefix for output json file
+file_string = "LiH_r_1.72_fci_6_311Gss"
+
+# create template for molecular geometry
+mol_str = """
+Li
+H 1 1.72
+0 1
+symmetry c1
+"""
+
+# specify basic input parameters for the electronic structure calculation
+options_dict = {
+        "basis": "6-311G**",
+        "scf_type": "pk",
+        "e_convergence": 1e-12,
+        "d_convergence": 1e-12,
+}
+
+# specify input parameters unique to the cavity quantum electrodynamics calculation
+cavity_options = {
+    'molecule_id' : "LiH",
+    'omega_value' : 0.0,
+    'lambda_vector' : [0, 0, 0.00],
+    'ci_level' : 'fci',
+    'ignore_coupling' : False,
+    'number_of_photons' : 0,
+    'natural_orbitals' : False,
+    'photon_number_basis' : True,
+    'canonical_mos' : True,
+    'coherent_state_basis' : False,
+    'davidson_roots' : 6,
+    'davidson_threshold' : 1e-5,
+    'davidson_indim':20,
+    'davidson_maxdim':100,
+    'davidson_maxiter':100,
+    'davidson_indim':8,
+    'test_mode': False,
+}
+
+# create new dictionary to store calculation data
+calculation_data = {
+    "molecular_geometry" : {
+        "z-matrix" : [],
+    },
+    "return_results" : {
+        "fci_energies" : [],
+        "dipole_array": []
+    }
+}
+
+# store cavity and standard electronic structure data
+calculation_data.update(cavity_options)
+calculation_data.update(options_dict)
+
+# run calculation
+test_pf = PFHamiltonianGenerator(
+        mol_str,
+        options_dict,
+        cavity_options)
+
+energy_values = test_pf.CIeigs.tolist()
+dipole_values = test_pf.dipole_array.tolist()
+
+calculation_data["return_results"]["fci_energies"].append(energy_values)
+calculation_data["return_results"]["dipole_array"].append(dipole_values)
+
+# write dictionary to JSON file
+with open(f"{file_string}.json", "w") as f:
+    json.dump(calculation_data, f, indent=4)
+
---
+++ Mapol/MgHplus/QED-CASSCF/MgHplus_r_1.65_lz_0.01.py
@@ -0,0 +1,93 @@
+# set path to the directory where helper_PFCI.py is located
+import sys
+sys.path.append("/Users/jfoley19/Code/qed-ci/src")
+
+# import helper libraries
+import numpy as np
+import psi4
+from helper_PFCI import PFHamiltonianGenerator
+import json
+
+# set precision for printing numpy arrays
+np.set_printoptions(threshold=sys.maxsize)
+
+# create file prefix for output json file
+file_string = "MgHp_r_scan_ccpVDZ_qed_casscf_8e_12o_lamz_0.1_om_0.136"
+
+# create template for molecular geometry
+mol_str = """
+Mg
+H 1 1.65
+1 1
+symmetry c1
+"""
+
+# specify basic input parameters for the electronic structure calculation
+options_dict = {
+        "basis": "cc-pVDZ",
+        "scf_type": "pk",
+        "e_convergence": 1e-10,
+        "d_convergence": 1e-10,
+}
+
+# specify input parameters that are unique to the cavity quantum electrodynamics calculation
+cavity_options = {
+    'molecule_id' : "LiH",
+    'omega_value' : 0.136,
+    'lambda_vector' : [0, 0, 0.05],
+    'ci_level' : 'cas',
+    'ignore_coupling' : False,
+    'number_of_photons' : 10,
+    'natural_orbitals' : False,
+    'photon_number_basis' : False,
+    'canonical_mos' : False,
+    'coherent_state_basis' : True,
+    'davidson_roots' : 6,
+    'davidson_threshold' : 1e-5,
+    'davidson_indim':20,
+    'davidson_maxdim':100,
+    #'spin_adaptation': "singlet",
+    #'casscf_weight':np.array([1,1,1]),
+    'davidson_maxiter':100,
+    'davidson_indim':8,
+    'test_mode': False,
+    'nact_orbs' : 12,
+    'nact_els' : 8
+}
+
+# create new dictionary to store calculation data
+calculation_data = {
+
+    "molecular_geometry" : {
+        "z-matrix" : [],
+    },
+    "return_results" : {
+
+        "casci_energies" : [],
+        "casscf_energies" : []
+    }
+
+}
+# store cavity parameters in dictionary
+for keys, values in cavity_options.items():
+    calculation_data[keys] = values
+# store standard electronic structure data in dictionary
+for keys, values in options_dict.items():
+    calculation_data[keys] = values
+
+test_pf = PFHamiltonianGenerator(
+        mol_str,
+        options_dict,
+        cavity_options)
+# create random energy values for 3 different states
+casci_energy_values = test_pf.CIeigs.tolist()
+casscf_energy_values = test_pf.CASSCFeigs.tolist()
+# create random dipole values for 3 different states
+# add mol_str to the molecular geometry dictionary
+calculation_data["molecular_geometry"]["z-matrix"].append(mol_str)
+# add energy values to the return_results dictionary
+calculation_data["return_results"]["casci_energies"].append(casci_energy_values)
+calculation_data["return_results"]["casscf_energies"].append(casscf_energy_values)
+
+
+print(calculation_data)
